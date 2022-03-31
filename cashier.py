5826ab5134d133809a3a499fba74e55629f878c7
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2022-03-30 17:56:05 -0700

    solution

--- cashier.py
+++ cashier.py
@@ -1,39 +1,43 @@
 def get_invoice_items(items):
-    # Items is a dictionary with a quantity and price key, and a name key
-    # Return a list of all the invoice line items in the following format:
-    # quantity name subtotal currency
-    # For example, if we had the following:
-    # [
-    #   {'name': 'Apple', 'quantity': 1, price: 0.2 },
-    #   {'name': 'Orange', 'quantity': 4, price: 0.3 },
-    # ]
-    # We should return the following:
-    # ['1 Apple 0.200KD', '4 Orange 1.200KD']
-    # ---
-    # Write your code here
-    ...
+    invoice_items = []
+    for item in items:
+        subtotal = item["price"] * item["quantity"]
+        name = item["name"]
+        quantity = item["quantity"]
+        invoice_items.append(f"{quantity} {name}  {subtotal} KD")
+    return invoice_items
 
 
 def get_total(items):
-    # Items is a dictionary with a quantity and price key
-    # Calculate the total of all items in the cart
-    # Write your code here
-    ...
+    total = 0
+    for item in items:
+        itemSubtotal = item["price"] * item["quantity"]
+        total += itemSubtotal
+    return total
 
 
 def print_receipt(invoice_items, total):
-    # invoice_items will be the list of formatted items received from
-    # `get_invoice_items`, and total will be a float. Print out a nice receipt
-    # displaying a title, all the invoice items on separate lines, and the
-    # total at the end.
-    # ---
-    # Write your code here
-    ...
+    print("Here's you receipt sir 👨🏼‍🍳")
+    print("-" * 20)
+    print(*invoice_items, sep="\n")
+    print("-" * 20)
+    print(total)
 
 
 def main():
-    # Write your main logic here
-    ...
+    items = []
+    item_name = input("Enter an item : ")
+
+    while item_name != "done":
+        price = float(input("Price: "))
+        quantity = int(input("Quantity: "))
+        items.append({"name": item_name, "price": price, "quantity": quantity})
+
+        item_name = input("Item (enter 'done' when finished): ")
+
+    invoice_items = get_invoice_items(items)
+    total = get_total(items)
+    print_receipt(invoice_items, total)
 
 
 if __name__ == "__main__":
